2ba59a851c68e1b0c1beee812a6281aac6babb36
techysj <40802503+techysj@users.noreply.github.com>
2021-07-03 01:04:34 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+const inputs = document.querySelectorAll('.controls input');
+ function handleInputs() {
+     const suffix= this.dataset.sizing || '';
+     document.documentElement.style.setProperty(`--${this.name}`,this.value + suffix);
+    //  console.log(`--${this.name}`,this.value + suffix)
+ }
+
+
+ inputs.forEach(input =>input.addEventListener('change' , handleInputs))
+ inputs.forEach(input =>input.addEventListener('mousemove' , handleInputs))
